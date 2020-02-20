582fec8dea6bbb9d9554dd0de9ce7d160a65cd52
SANGHARSHSOVITKAR <46445941+SANGHARSHSOVITKAR@users.noreply.github.com>
2020-02-20 19:16:26 +0530

    v8
    v8

---
+++ precache-manifest.c7a3054d58a0f6ea4bf2244c841ed4f2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "688675ba6d1b2efe51411bff6ceef0e0",
+    "url": "./index.html"
+  },
+  {
+    "revision": "661d64f022ac7279c5dd",
+    "url": "./static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "1f3ab3cbbe53ff3420f7",
+    "url": "./static/css/main.fb8eb724.chunk.css"
+  },
+  {
+    "revision": "661d64f022ac7279c5dd",
+    "url": "./static/js/2.9a062cc2.chunk.js"
+  },
+  {
+    "revision": "1f3ab3cbbe53ff3420f7",
+    "url": "./static/js/main.e41fe618.chunk.js"
+  },
+  {
+    "revision": "3e455c1c8fa4d002b0e6",
+    "url": "./static/js/runtime-main.88d1c77f.js"
+  }
+]);
